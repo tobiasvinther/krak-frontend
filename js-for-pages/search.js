27d14e860c693fe9288ad0951e1fe57e7ef3dfbf
Thony <thon0101@stud.kea.dk>
2022-03-18 16:36:07 +0100

    Search: kan nu få dummydata hobby & beskrivelse op på layoutet

--- js-for-pages/search.js
+++ js-for-pages/search.js
@@ -4,7 +4,10 @@ import{SERVER}from"../settings.js"
 const URL = SERVER+"/hobby"
 let myPersonData = []
 
+//document.getElementById("search-button").onclick = getPersonData
+
 export function getPersonData(){
+    document.getElementById("search-button").onclick = findUser
     if(myPersonData.length > 0){
         makeRows(myPersonData)
         return
@@ -12,18 +15,33 @@ export function getPersonData(){
     fetch(URL)
         .then(res => res.json())
         .then(hobby => {
-            makeRows(hobby)
+            //makeRows(hobby)
             myPersonData = hobby
         })
+        .then( () => {
+            document.getElementById("search-button").onclick = findUser
+        })
         .catch(e => console.error(e))
+}
+
+function findUser(evt) {
+    evt.preventDefault();
+    console.log("test")
+    const result = myPersonData.filter(hobby => {
+        return hobby.name === document.getElementById("search").value //Phone i stedet for navn
+    })
+    makeRows(result)
 }
 
 function makeRows(rows){
     const trows =  rows.map(hobby =>`
     <tr>
-        <td>Person</td>
-        <td>Adress</td> 
-        <td>Phone</td>   
+        <td>Firstname</td>
+        <td>Lastname</td> 
+        <td>Phone</td>
+        <td>Email</td>
+        <td>Adress</td>
+        <td>City</td>   
         <td>${encode(hobby.name)}</td>
         <td>${encode(hobby.description)}</td>
     </tr>
