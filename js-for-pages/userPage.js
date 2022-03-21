5a5eb7fe2c4357a1c8ca300712da62052cf4ffb3
tobiasvinther <tobi626i@stud.kea.dk>
2022-03-21 19:56:16 +0100

    still trying to get adding hobbies to user to work

--- js-for-pages/userPage.js
+++ js-for-pages/userPage.js
@@ -1,6 +1,7 @@
 import {SERVER} from "../settings.js"
 import {encode} from "../utils.js"
-const URL = SERVER + ""
+import {makeOptions} from "../fetchUtils.js";
+const URL = SERVER
 
 
 export function getUser() {
@@ -20,6 +21,18 @@ export function getUser() {
             document.getElementById("id-phone").innerText = data.phoneNumber
             document.getElementById("id-city").innerText = data.city
 
+
+            //create a list of the person's hobbies
+            /*
+            document.getElementById("id-hobby-list").innerHTML = data.map(person =>
+            `
+                <li>${encode(person.hobbies)}</li>
+            `
+            ).join("\n")
+
+             */
+
+
         })
         .catch(err => {
             //document.getElementById("id-error").innerText = "User doesn't exist"
@@ -28,51 +41,79 @@ export function getUser() {
         .finally(e => console.log("End: getUser"))
 }
 
-let hobbieList = []
+let hobbyList = []
 
 export function addHobbySubmit(){
     document.getElementById("add-hobby").onclick= addHobby
 }
 
-function addHobby() {
+//get this to work...
+
+function addHobby1() {
     let select = document.getElementById('hobby-select1');
     let value = select.options[select.selectedIndex].value;
+    const name = select.options[select.selectedIndex].innerText;
     console.log(value);
 
-    fetch("/persons/user1/add-hobby", {
+    fetch(URL + "/persons/user1/add-hobby", {
         method: "POST",
         headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Accept": "application/json"
         },
         body: JSON.stringify({
-            id: "1",
-            hobbyName: "Tennis",
-            description: "Tennis er en fed sport"
+            id: value,
+            hobbyName: name
         })
     });
 }
 
+
+//or this... maybe the issue is that nothing is returned?
+function addHobby(){
+
+    const select = document.getElementById('hobby-select1');
+    const value = select.options[select.selectedIndex].value;
+    const name = select.options[select.selectedIndex].innerText;
+
+    const hobby ={}
+
+    hobby.id = value
+    hobby.hobbyName = name
+
+    fetch(URL + "/persons/user1/add-hobby", makeOptions("POST", hobby, false))
+        .then(res=>{
+            if(!res.ok){
+                return Promise.reject("Error: " + res.status)
+            }
+            return res.json()
+        })
+        .catch(e => console.error(e))
+}
+
+
+
 export function getAllHobbies(){
     console.log("Start: getAllHobbies")
-    if(hobbieList.length > 0){
-        makeRows(hobbieList)
+    if(hobbyList.length > 0){
+        makeRows(hobbyList)
         return
     }
     fetch(URL + "/hobby")
         .then(res => res.json())
         .then(hobbies => {
             makeRows(hobbies)
-            hobbieList = hobbies
+            hobbyList = hobbies
         })
         .catch(e => console.error(e))
 }
 
 function makeRows(rows){
     const defaultSelect =  document.getElementById("hobby-select1").innerHTML
+
     document.getElementById("hobby-select1").innerHTML = defaultSelect + rows.map(hobby =>
     `
-        <option>${encode(hobby.name)}</option>
+        <option value="${hobby.id}">${encode(hobby.name)}</option>
     `
     ).join("\n")
 }
