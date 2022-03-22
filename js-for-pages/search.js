b66bbabe39ff3c137cb4c16de01543fd2c8d6a78
Thony <thon0101@stud.kea.dk>
2022-03-22 12:00:00 +0100

    Search frontend: Hidden/show result added to search

--- js-for-pages/search.js
+++ js-for-pages/search.js
@@ -1,22 +1,22 @@
 import{encode}from"../utils.js"
 import{SERVER}from"../settings.js"
 
-const URL = SERVER+"/hobby"
+const URL = SERVER+"/persons"
 let myPersonData = []
 
 //document.getElementById("search-button").onclick = getPersonData
 
 export function getPersonData(){
     document.getElementById("search-button").onclick = findUser
-    if(myPersonData.length > 0){
+    /*if(myPersonData.length > 0){
         makeRows(myPersonData)
         return
-    }
+    }*/
     fetch(URL)
         .then(res => res.json())
-        .then(hobby => {
-            //makeRows(hobby)
-            myPersonData = hobby
+        .then(person => {
+            //makeRows(person)
+            myPersonData = person
         })
         .then( () => {
             document.getElementById("search-button").onclick = findUser
@@ -27,23 +27,24 @@ export function getPersonData(){
 function findUser(evt) {
     evt.preventDefault();
     console.log("test")
-    const result = myPersonData.filter(hobby => {
-        return hobby.name === document.getElementById("search").value //Phone i stedet for navn
+    const result = myPersonData.filter(person => {
+        return person.firstName === document.getElementById("search-input").value || //lastName i stedet for firstName vil gøre at man kan søge på lastName i stedet for.
+            person.email === document.getElementById("search-input").value //Gør at man kan søge på email
     })
+    console.log(result)
     makeRows(result)
 }
 
-function makeRows(rows){
-    const trows =  rows.map(hobby =>`
+function makeRows(person){
+    //Gør at table bliver synligt ved søgning af person og forsvinder igen ved ny søgning.
+    document.getElementById("table").style.display = "block"
+    const trows = person.map(p =>`
     <tr>
-        <td>Firstname</td>
-        <td>Lastname</td> 
-        <td>Phone</td>
-        <td>Email</td>      
-        <td>Adress</td> 
-        <td>City</td> 
-        <td>${encode(hobby.name)}</td>
-        <td>${encode(hobby.description)}</td>
+        <td>${encode(p.firstName)}</td>
+        <td>${encode(p.lastName)}</td> 
+        <td>${encode(p.email)}</td>      
+        <td>${encode(p.city)}</td>
+        <td>${encode(p.hobbyName)}</td> 
     </tr>
     `).join("\n")
     document.getElementById("search-body").innerHTML = trows
